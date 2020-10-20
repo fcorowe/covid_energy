af5f3ed6abd7060ab09c9a66a6065f4a3aeef747
fcorowe <fcorowe@gmail.com>
2020-10-20 10:52:25 +0100

    add model code

--- methods/modelling.R
+++ methods/modelling.R
@@ -3,41 +3,40 @@ library(tidyverse)
 
 rm(list=ls())
 
-# read case data
+#######
+# 1. Read data
+
+# COVID data
 case_data <- read.csv('../data/for_code/owid-covid-data.csv')
 
-
-
-# read the file with the cities included in the analysis
+# list of cities
 cities_data <- read.csv("../data/for_code/UN_Cities_names.csv")
 
+# mobility data
+mob_data <- read.csv("../data/for_code/mobility_and_stringency_uncities.csv")
 
-# define a vector with city names
+#######
+# 2. Data wrangling
+
+## 2.1 COVID data
+# define a vector of city names
 cities <- as.vector(cities_data[,1])
 
-# define a vector with country names
+# define a vector of country names
 country <- countrycode(cities_data[,2], "country.name", "iso3c")
 
-
-
-
-# make sure that I exclude Hong Kong as we assume that it is reported with China
+# exclude Hong Kong - assume reported with China
 case_data_subset <- subset(case_data, iso_code %in% unique(country)[-grep("HKG", unique(country))])
 
-# include dates only up to 
-case_data_subset <- subset(case_data_subset, date <= as.Date("2020-03-31") )
+# restrict period of analysis to end of March
+case_data_subset <- subset(case_data_subset, date <= as.Date("2020-04-30") )
 
-# convert the date to the correct format
+# format dates
 case_data_subset$date <- as.Date(case_data_subset$date)
 
-# keep only the columns needed
-case_data_subset <- case_data_subset[,c("iso_code", "location", "date", "new_cases", "new_deaths")]
 
 
-# read in mobility data ---------------------------------------------------
-
-# read mobility data
-mob_data <- read.csv("../data/for_code/mobility_and_stringency_uncities.csv")
+## 2.2. Mobility data
 
 # attach the country code
 mob_data$iso_code <- countrycode(mob_data[,2], "country.name", "iso3c")
@@ -46,15 +45,45 @@ mob_data$iso_code <- countrycode(mob_data[,2], "country.name", "iso3c")
 mob_data$Date <- as.Date(mob_data$Date, "%d/%m/%Y")
 
 # set the maximum date to end of March
-mob_data <- subset(mob_data, Date <= as.Date("2020-03-31") )
+mob_data <- subset(mob_data, Date <= as.Date("2020-04-30") )
 
 # extract the month
 mob_data$month <- strftime(mob_data$Date, "%m")
 
+# select variable set
+mob_data <- mob_data %>%
+  select(-c(4)) 
 
-# combine cases and mobility data
-combined_data <- left_join(mob_data, case_data_subset, by = c("iso_code" = "iso_code", "Date" = "date"))
+## 2.3 Create a single data frame
+cdf <- left_join(case_data_subset, mob_data,by = c("iso_code" = "iso_code", "date" = "Date"))
+
+## 2.4 Restrict sample to 14/02 when mobility data start
+cdf <- cdf %>% filter(date > "2020-02-14")
+
+## 2.5 Creating additional variables
+# growth rate
+cdf <- cdf %>% group_by(City) %>% 
+  filter(total_cases > 0) %>% 
+  mutate(t_cases_lag = lag(total_cases, n=1, default = NA))
+
+cdf <- cdf %>% mutate(gr_cases = ( (total_cases - t_cases_lag) / t_cases_lag) *100 )
+
+# doubling time
+cdf <- cdf %>% mutate(dng_time = log(2) / log( 1 + gr_cases) ) 
+cdf$dng_time[is.infinite(cdf$dng_time)] <- 0
+
+# troubleshooting
+#cdf[1:15, c(3, 4, 5, 6, 49, 46)]
+#cdf[85:100, c(3, 4, 5, 6, 49, 46)]
+
+#######
+# 3. Exploratory data analysis
+
+## 3.1. N of observations by city
+table(cdf$City)
+
+## 3.2 Autocorrelation in mobility
 
 
-
-
+#######
+# 4. Multilevel modelling
