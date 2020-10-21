e6feacabbdf0997d2308925e1d0fd9432bdd72ba
fcorowe <fcorowe@gmail.com>
2020-10-21 11:55:17 +0100

    model code update

--- methods/modelling.R
+++ methods/modelling.R
@@ -3,8 +3,20 @@ library(tidyverse)
 library(timetk)
 library(ggpubr)
 library(lme4)
+library(ggcorrplot)
+library(corrplot)
+library(viridis)
+library(sf)
 
 rm(list=ls())
+
+# Google’s COVID-19 Community Mobility Reports show how visits and length of stay in 
+# various place categories have changed compared to a baseline period before the pandemic.
+
+# we focus on the residential category, which is defined as the time users spent at home, 
+# using the home addresses provided to or estimated by Google Maps. 
+# Our focus on a variable related to time use and duration of events is consistent 
+# with the epidemiological literature on time use and the spread of close-contact infectious diseases.
 
 #######
 # 1. Read data
@@ -72,9 +84,9 @@ cdf <- cdf %>% filter(date > "2020-02-14")
 # growth rate
 cdf <- cdf %>% group_by(City) %>% 
   filter(total_cases > 0) %>% 
-  mutate(t_cases_lag = lag(total_cases, n=1, default = NA))
+  mutate(cases_t1lag = lag(total_cases, n=1, default = NA))
 
-cdf <- cdf %>% mutate(gr_cases = ( (total_cases - t_cases_lag) / t_cases_lag) *100 )
+cdf <- cdf %>% mutate(gr_cases = ( (total_cases - cases_t1lag) / cases_t1lag) *100 )
 
 # doubling time: time taken for a population to double in size
 cdf <- cdf %>% mutate(dng_time = log(2) / log( 1 + gr_cases) ) 
@@ -103,7 +115,7 @@ table(cdf$City)
 ## 3.2 Autocorrelation in mobility
 p1 <- cdf %>% dplyr::filter(as.integer(City) %in% c(1:12)) %>% 
   group_by(City) %>% 
-  plot_acf_diagnostics(date, Workplaces, 
+  plot_acf_diagnostics(date, Residential, 
                        .lags = "14 days",
                        .line_size = 2,
                        .show_white_noise_bars = TRUE,
@@ -112,7 +124,7 @@ p1 <- cdf %>% dplyr::filter(as.integer(City) %in% c(1:12)) %>%
 
 p2 <- cdf %>% dplyr::filter(as.integer(City) %in% c(12:20)) %>% 
   group_by(City) %>% 
-  plot_acf_diagnostics(date, Workplaces, 
+  plot_acf_diagnostics(date, Residential, 
                        .lags = "14 days",
                        .line_size = 2,
                        .show_white_noise_bars = TRUE,
@@ -121,7 +133,7 @@ p2 <- cdf %>% dplyr::filter(as.integer(City) %in% c(12:20)) %>%
 
 p3 <- cdf %>% dplyr::filter(as.integer(City) %in% c(21:29)) %>% 
   group_by(City) %>% 
-  plot_acf_diagnostics(date, Workplaces, 
+  plot_acf_diagnostics(date, Residential, 
                        .lags = "14 days",
                        .line_size = 2,
                        .show_white_noise_bars = TRUE,
@@ -130,7 +142,7 @@ p3 <- cdf %>% dplyr::filter(as.integer(City) %in% c(21:29)) %>%
 
 p4 <- cdf %>% dplyr::filter(as.integer(City) %in% c(30:38)) %>% 
   group_by(City) %>% 
-  plot_acf_diagnostics(date, Workplaces, 
+  plot_acf_diagnostics(date, Residential, 
                        .lags = "14 days",
                        .line_size = 2,
                        .show_white_noise_bars = TRUE,
@@ -139,7 +151,7 @@ p4 <- cdf %>% dplyr::filter(as.integer(City) %in% c(30:38)) %>%
 
 p5 <- cdf %>% dplyr::filter(as.integer(City) %in% c(39:47)) %>% 
   group_by(City) %>% 
-  plot_acf_diagnostics(date, Workplaces, 
+  plot_acf_diagnostics(date, Residential, 
                        .lags = "14 days",
                        .line_size = 2,
                        .show_white_noise_bars = TRUE,
@@ -148,7 +160,7 @@ p5 <- cdf %>% dplyr::filter(as.integer(City) %in% c(39:47)) %>%
 
 p6 <- cdf %>% dplyr::filter(as.integer(City) %in% c(48:50)) %>% 
   group_by(City) %>% 
-  plot_acf_diagnostics(date, Workplaces, 
+  plot_acf_diagnostics(date, Residential, 
                        .lags = "14 days",
                        .line_size = 2,
                        .show_white_noise_bars = TRUE,
@@ -181,7 +193,100 @@ dev.off()
 rm(p1,p2,p3,p4,p5,p6)
 
 
+# To do list
+#  * create a correlogram for each city
+#  * create lags at t0, t7, t14 for COVID cases, deaths and stringency
+#  * create scatter plots bwt mobility vs COVID cases, deaths and stringency at lags t1, t7 and t14
+#  * add spline line mobility variable
+#  * run models:
+#      * 
+
+## 
+
+plotList <- list()
+plotList2 <- list()
+
+# we run this code for two groups of  25 cities
+
+for (j in levels(cdf$City)[1:25]) {
+  
+  plotList[[1]] <- ggplot(cdf, aes(x = date, y = measure, colour= variable)) +
+    geom_smooth(method = "loess", se = FALSE, size=1, span = 0.3) +
+    xlab("") +
+    ylab("") +
+    theme_minimal() +
+    theme(text = element_text(size = 20),
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          legend.position = "none",
+          plot.margin=unit(c(0.1,0.1,-0.5,0.1), "cm")) + # set the bottom margin to negative
+    scale_color_manual(values = cpal) +
+    guides(color=guide_legend(""))
+  
+  plotList[[2]] <- ggplot(data4, aes(x = Date, y = Residential, colour= "#74787b")) +
+    geom_smooth(method = "loess", se = FALSE, size=1, span = 0.3) +
+    xlab("") +
+    ylab("") +
+    theme_minimal() +
+    theme(text = element_text(size = 20),
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          legend.position = "none",
+          plot.margin=unit(c(-0.5,0.1,0.1,0.1), "cm")) + # set the upper margin to negative
+    scale_color_manual(values = cpal) +
+    guides(color=guide_legend(""))
+  
+  
+  
+  
+  
+  plotList2[[j]] <- annotate_figure(ggarrange(plotlist = plotList, ncol=1, nrow=2), 
+                                    top = text_grob(j, size = 10))
+  
+}
+
+
+png("mobility_plot_v1.png",units="in", width=10, height=10, res=300)
+ggarrange(plotlist = plotList2, ncol=5, nrow=5)
+dev.off() #
+
+
 ## 3.4 Correlation
+    ### Full sample
+pc <- cor( cdf[ , c("Residential", 
+                    "Workplaces", 
+                    "new_cases_per_million",
+                    "cases_t1lag",
+                    "gr_cases",
+                    "dng_time",
+                    "new_deaths_per_million",
+                    "stringency_index",
+                    "population_density",
+                    "gdp_per_capita",
+                    "aged_65_older",
+                    "cardiovasc_death_rate",
+                    "life_expectancy") ], 
+          use = "complete.obs",
+           method="pearson" )
+
+# Change labels
+colnames(pc) <- c("Stay-at-home", "Workplace", "New cases", "New cases t-1", "Cases growth rate", "Cases doubling time", "Deaths", "Stringency", "Pop density", "GDP", "Pop 65+", "Cardiovascular death", "Life expectancy")
+rownames(pc) <- c("Stay-at-home", "Workplace", "New cases", "New cases t-1", "Cases growth rate", "Cases doubling time", "Deaths", "Stringency", "Pop density", "GDP", "Pop 65+", "Cardiovascular death", "Life expectancy")
+
+# significance test
+sig <- corrplot::cor.mtest(pc, conf.level = .95)
+
+# create a correlogram
+corrplot::corrplot(pc, type="lower",
+                   method = "circle", 
+                   order = "original", 
+                   tl.cex = 0.7,
+                   p.mat = sig$p, sig.level = .05, 
+                   col = viridis::viridis(100, option = "plasma"),
+                   diag = FALSE)
+
+
+   ### By city
 corr <- cdf %>%
   group_by(City) %>% 
   dplyr::summarise(r = cor(Workplaces, 
@@ -190,6 +295,11 @@ corr <- cdf %>%
                     new_deaths_per_million, 
                     stringency_index))
 
+
+vars_keep <- names(cdf)[c("Workplaces", "Residential", "new_cases_per_million")]
+some <- cdf %>% split(.$City) %>% 
+  map(select, vars_keep) %>%
+  map(cor)
 
 vars_keep <- names(mtcars)[c(1, 3, 4)]
 some <- mtcars %>% split(.$cyl) %>% map(select, vars_keep) %>% map(cor)
@@ -220,3 +330,21 @@ model2 <- lmer(eq1, data = cdf)
 # estimates
 summary(model2)
 
+
+
+
+
+
+use this last line of code 
+
+
+# read the FUAs polygons
+fua <- st_read("../data/for_code/FUA_layer/GHS_FUA_UCDB2015_GLOBE_R2019A_54009_1K_V1_0_MERGED.gpkg")
+WGS84 = "+init=epsg:4326"
+# make sure that all layers have consistent CRS- in this case is WGS84
+fua <- st_transform(fua, WGS84)
+
+
+
+# this is population density by sqkm
+fua$pop_dens <- fua$FUA_p_2015 /  fua$FUA_area
